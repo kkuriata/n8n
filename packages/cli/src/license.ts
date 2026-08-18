8fd2790867f61007664bbe089ff0d8cb10ac9e42
Krzysztof Kuriata <krzysztof@kuriata.me>
2026-08-18 20:15:30 +0200

    License turned off

--- packages/cli/src/license.ts
+++ packages/cli/src/license.ts
@@ -278,7 +278,8 @@ export class License implements LicenseProvider {
 	}
 
 	isLicensed(feature: BooleanLicenseFeature) {
-		return this.manager?.hasFeatureEnabled(feature) ?? false;
+		// return this.manager?.hasFeatureEnabled(feature) ?? false;
+		return true;
 	}
 
 	isCertValid(): boolean {
